9afc7942ec3339335ddebeb0423f44a219fe843b
Grace Park <gracepark@github.com>
2017-09-14 13:21:59 -0700

    ajax try

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("#start").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "game.html", true);
+  xhttp.send();
+}
 // Display the page with the start button
 $(document).ready(function () {
-	function loadDoc() {
-	  var xhttp = new XMLHttpRequest();
-	  xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	     document.getElementById("#start").innerHTML = this.responseText;
-	    }
-	  };
-	  xhttp.open("GET", "game.html", true);
-	  xhttp.send();
-	}
+
 });
 
 
