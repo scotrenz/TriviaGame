e4ff21e614ceda2caf956e041ed1749cd2c70929
Grace Park <gracepark@github.com>
2017-09-14 14:55:22 -0700

    css change for background

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,4 @@
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("#start").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "game.html", true);
-  xhttp.send();
-}
+
 // Display the page with the start button
 $(document).ready(function () {
 
